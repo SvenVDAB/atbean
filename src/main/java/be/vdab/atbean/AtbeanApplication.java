2d83be20f541cc59459ebf0fd49c0f27ee94f0de
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-01-30 11:52:06 +0100

    16 not given

--- src/main/java/be/vdab/atbean/AtbeanApplication.java
+++ src/main/java/be/vdab/atbean/AtbeanApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AtbeanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtbeanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AtbeanApplication.class, args);
+    }
 
 }
